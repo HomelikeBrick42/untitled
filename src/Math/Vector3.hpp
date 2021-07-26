ab0b2ce95143dd28f52dad72a4e21128c6c7a128
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-07-26 20:20:35 +1200

    Added Vector3 operators

--- src/Math/Vector3.hpp
+++ src/Math/Vector3.hpp
@@ -2,8 +2,18 @@
 
 #include "Core/Defines.hpp"
 
+#include <cmath>
+
+template<u64 Length, typename T>
+struct Vector;
+
 template<typename T>
-struct Vector3 {
+using Vector3 = Vector<3, T>;
+
+using Vector3f = Vector3<f32>;
+
+template<typename T>
+struct Vector<3, T> {
     union {
         struct {
             T x;
@@ -17,11 +27,214 @@ struct Vector3 {
         };
     };
 
-    Vector3() : x(T()), y(T()), z(T()) {}
+    Vector<3, T>() : x(T()), y(T()), z(T()) {}
 
-    Vector3(T scalar) : x(scalar), y(scalar), z(scalar) {}
+    Vector<3, T>(const T& scalar) : x(scalar), y(scalar), z(scalar) {}
 
-    Vector3(T x, T y, T z) : x(x), y(y), z(y) {}
+    Vector<3, T>(const T& x, const T& y, const T& z) : x(x), y(y), z(z) {}
+
+    T& operator[](u64 index) {
+        switch (index) {
+            case 0:
+                return this->x;
+            case 1:
+                return this->y;
+            case 2:
+                return this->z;
+            default:
+                // TODO: Error
+                return this->x;
+        }
+    }
+
+    const T& operator[](u64 index) const {
+        switch (index) {
+            case 0:
+                return this->x;
+            case 1:
+                return this->y;
+            case 2:
+                return this->z;
+            default:
+                // TODO: Error
+                return this->x;
+        }
+    }
+
+    static T Dot(const Vector3<T>& a, const Vector3<T>& b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    static Vector3<T> Cross(const Vector3<T>& a, const Vector3<T>& b) {
+        return {
+            a.y * b.z - a.z * b.y,
+            a.z * b.x - a.x * b.z,
+            a.x * b.y - a.y * b.x,
+        };
+    }
+
+    static Vector3<T> Normalise(const Vector3<T>& v) {
+        T length = static_cast<T>(sqrt(static_cast<f64>(Vector3<T>::Dot(v, v))));
+        if (length != T()) {
+            return v / length;
+        } else {
+            return Vector3<T>();
+        }
+    }
 };
 
-using Vector3f = Vector3<f32>;
+template<typename T>
+Vector3<T> operator+(const Vector3<T>& a, const Vector3<T>& b) {
+    return {
+        a.x + b.x,
+        a.y + b.y,
+        a.z + b.z,
+    };
+}
+
+template<typename T>
+Vector3<T>& operator+=(Vector3<T>& a, const Vector3<T>& b) {
+    a = a + b;
+    return a;
+}
+
+template<typename T>
+Vector3<T> operator+(const Vector3<T>& a, const T& b) {
+    return {
+        a.x + b,
+        a.y + b,
+        a.z + b,
+    };
+}
+
+template<typename T>
+Vector3<T> operator+(const T& a, const Vector3<T>& b) {
+    return {
+        a + b.x,
+        a + b.y,
+        a + b.z,
+    };
+}
+
+template<typename T>
+Vector3<T> operator+=(Vector3<T>& a, const T& b) {
+    a = a + b;
+    return a;
+}
+
+template<typename T>
+Vector3<T> operator-(const Vector3<T>& a, const Vector3<T>& b) {
+    return {
+        a.x - b.x,
+        a.y - b.y,
+        a.z - b.z,
+    };
+}
+
+template<typename T>
+Vector3<T>& operator-=(Vector3<T>& a, const Vector3<T>& b) {
+    a = a - b;
+    return a;
+}
+
+template<typename T>
+Vector3<T> operator-(const Vector3<T>& a, const T& b) {
+    return {
+        a.x - b,
+        a.y - b,
+        a.z - b,
+    };
+}
+
+template<typename T>
+Vector3<T> operator-(const T& a, const Vector3<T>& b) {
+    return {
+        a - b.x,
+        a - b.y,
+        a - b.z,
+    };
+}
+
+template<typename T>
+Vector3<T> operator-=(Vector3<T>& a, const T& b) {
+    a = a - b;
+    return a;
+}
+
+template<typename T>
+Vector3<T> operator*(const Vector3<T>& a, const Vector3<T>& b) {
+    return {
+        a.x * b.x,
+        a.y * b.y,
+        a.z * b.z,
+    };
+}
+
+template<typename T>
+Vector3<T>& operator*=(Vector3<T>& a, const Vector3<T>& b) {
+    a = a * b;
+    return a;
+}
+
+template<typename T>
+Vector3<T> operator*(const Vector3<T>& a, const T& b) {
+    return {
+        a.x * b,
+        a.y * b,
+        a.z * b,
+    };
+}
+
+template<typename T>
+Vector3<T> operator*(const T& a, const Vector3<T>& b) {
+    return {
+        a * b.x,
+        a * b.y,
+        a * b.z,
+    };
+}
+
+template<typename T>
+Vector3<T> operator*=(Vector3<T>& a, const T& b) {
+    a = a * b;
+    return a;
+}
+
+template<typename T>
+Vector3<T> operator/(const Vector3<T>& a, const Vector3<T>& b) {
+    return {
+        a.x / b.x,
+        a.y / b.y,
+        a.z / b.z,
+    };
+}
+
+template<typename T>
+Vector3<T>& operator/=(Vector3<T>& a, const Vector3<T>& b) {
+    a = a / b;
+    return a;
+}
+
+template<typename T>
+Vector3<T> operator/(const Vector3<T>& a, const T& b) {
+    return {
+        a.x / b,
+        a.y / b,
+        a.z / b,
+    };
+}
+
+template<typename T>
+Vector3<T> operator/(const T& a, const Vector3<T>& b) {
+    return {
+        a / b.x,
+        a / b.y,
+        a / b.z,
+    };
+}
+
+template<typename T>
+Vector3<T> operator/=(Vector3<T>& a, const T& b) {
+    a = a / b;
+    return a;
+}
