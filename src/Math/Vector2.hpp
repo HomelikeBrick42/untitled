71fd3c75b5fc8bb1348381b6d6a8e1d28817c0b4
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-07-27 12:00:22 +1200

    Added Vector2 and Vector4

--- src/Math/Vector2.hpp
+++ src/Math/Vector2.hpp
@@ -6,28 +6,26 @@
 #include <cmath>
 
 template<typename T>
-using Vector3 = Vector<3, T>;
+using Vector2 = Vector<2, T>;
 
-using Vector3f = Vector3<f32>;
+using Vector2f = Vector2<f32>;
 
 template<typename T>
-struct Matrix<3, 1, T> {
+struct Matrix<2, 1, T> {
     union {
         struct {
             T x;
             T y;
-            T z;
         };
         struct {
             T r;
             T g;
-            T b;
         };
     };
 
-    Matrix<3, 1, T>() : x(T()), y(T()), z(T()) {}
-    Matrix<3, 1, T>(const T& scalar) : x(scalar), y(scalar), z(scalar) {}
-    Matrix<3, 1, T>(const T& x, const T& y, const T& z) : x(x), y(y), z(z) {}
+    Matrix<2, 1, T>() : x(T()), y(T()) {}
+    Matrix<2, 1, T>(const T& scalar) : x(scalar), y(scalar) {}
+    Matrix<2, 1, T>(const T& x, const T& y) : x(x), y(y) {}
 
     T& operator[](u64 index) {
         switch (index) {
@@ -35,8 +33,6 @@ struct Matrix<3, 1, T> {
                 return this->x;
             case 1:
                 return this->y;
-            case 2:
-                return this->z;
             default:
                 // TODO: Error
                 return this->x;
@@ -49,188 +45,166 @@ struct Matrix<3, 1, T> {
                 return this->x;
             case 1:
                 return this->y;
-            case 2:
-                return this->z;
             default:
                 // TODO: Error
                 return this->x;
         }
     }
 
-    static T Dot(const Vector3<T>& a, const Vector3<T>& b) {
-        return a.x * b.x + a.y * b.y + a.z * b.z;
+    static T Dot(const Vector2<T>& a, const Vector2<T>& b) {
+        return a.x * b.x + a.y * b.y;
     }
 
-    static Vector3<T> Cross(const Vector3<T>& a, const Vector3<T>& b) {
-        return {
-            a.y * b.z - a.z * b.y,
-            a.z * b.x - a.x * b.z,
-            a.x * b.y - a.y * b.x,
-        };
-    }
-
-    static Vector3<T> Normalise(const Vector3<T>& v) {
-        T length = static_cast<T>(sqrt(static_cast<f64>(Vector3<T>::Dot(v, v))));
+    static Vector2<T> Normalise(const Vector2<T>& v) {
+        T length = static_cast<T>(sqrt(static_cast<f64>(Vector2<T>::Dot(v, v))));
         if (length != T()) {
             return v / length;
         } else {
-            return Vector3<T>();
+            return Vector2<T>();
         }
     }
 };
 
 template<typename T>
-Vector3<T> operator+(const Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T> operator+(const Vector2<T>& a, const Vector2<T>& b) {
     return {
         a.x + b.x,
         a.y + b.y,
-        a.z + b.z,
     };
 }
 
 template<typename T>
-Vector3<T>& operator+=(Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T>& operator+=(Vector2<T>& a, const Vector2<T>& b) {
     a = a + b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator+(const Vector3<T>& a, const T& b) {
+Vector2<T> operator+(const Vector2<T>& a, const T& b) {
     return {
         a.x + b,
         a.y + b,
-        a.z + b,
     };
 }
 
 template<typename T>
-Vector3<T> operator+(const T& a, const Vector3<T>& b) {
+Vector2<T> operator+(const T& a, const Vector2<T>& b) {
     return {
         a + b.x,
         a + b.y,
-        a + b.z,
     };
 }
 
 template<typename T>
-Vector3<T> operator+=(Vector3<T>& a, const T& b) {
+Vector2<T> operator+=(Vector2<T>& a, const T& b) {
     a = a + b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator-(const Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T> operator-(const Vector2<T>& a, const Vector2<T>& b) {
     return {
         a.x - b.x,
         a.y - b.y,
-        a.z - b.z,
     };
 }
 
 template<typename T>
-Vector3<T>& operator-=(Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T>& operator-=(Vector2<T>& a, const Vector2<T>& b) {
     a = a - b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator-(const Vector3<T>& a, const T& b) {
+Vector2<T> operator-(const Vector2<T>& a, const T& b) {
     return {
         a.x - b,
         a.y - b,
-        a.z - b,
     };
 }
 
 template<typename T>
-Vector3<T> operator-(const T& a, const Vector3<T>& b) {
+Vector2<T> operator-(const T& a, const Vector2<T>& b) {
     return {
         a - b.x,
         a - b.y,
-        a - b.z,
     };
 }
 
 template<typename T>
-Vector3<T> operator-=(Vector3<T>& a, const T& b) {
+Vector2<T> operator-=(Vector2<T>& a, const T& b) {
     a = a - b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator*(const Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T> operator*(const Vector2<T>& a, const Vector2<T>& b) {
     return {
         a.x * b.x,
         a.y * b.y,
-        a.z * b.z,
     };
 }
 
 template<typename T>
-Vector3<T>& operator*=(Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T>& operator*=(Vector2<T>& a, const Vector2<T>& b) {
     a = a * b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator*(const Vector3<T>& a, const T& b) {
+Vector2<T> operator*(const Vector2<T>& a, const T& b) {
     return {
         a.x * b,
         a.y * b,
-        a.z * b,
     };
 }
 
 template<typename T>
-Vector3<T> operator*(const T& a, const Vector3<T>& b) {
+Vector2<T> operator*(const T& a, const Vector2<T>& b) {
     return {
         a * b.x,
         a * b.y,
-        a * b.z,
     };
 }
 
 template<typename T>
-Vector3<T> operator*=(Vector3<T>& a, const T& b) {
+Vector2<T> operator*=(Vector2<T>& a, const T& b) {
     a = a * b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator/(const Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T> operator/(const Vector2<T>& a, const Vector2<T>& b) {
     return {
         a.x / b.x,
         a.y / b.y,
-        a.z / b.z,
     };
 }
 
 template<typename T>
-Vector3<T>& operator/=(Vector3<T>& a, const Vector3<T>& b) {
+Vector2<T>& operator/=(Vector2<T>& a, const Vector2<T>& b) {
     a = a / b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator/(const Vector3<T>& a, const T& b) {
+Vector2<T> operator/(const Vector2<T>& a, const T& b) {
     return {
         a.x / b,
         a.y / b,
-        a.z / b,
     };
 }
 
 template<typename T>
-Vector3<T> operator/(const T& a, const Vector3<T>& b) {
+Vector2<T> operator/(const T& a, const Vector2<T>& b) {
     return {
         a / b.x,
         a / b.y,
-        a / b.z,
     };
 }
 
 template<typename T>
-Vector3<T> operator/=(Vector3<T>& a, const T& b) {
+Vector2<T> operator/=(Vector2<T>& a, const T& b) {
     a = a / b;
     return a;
 }
--- src/Math/Vector4.hpp
+++ src/Math/Vector4.hpp
@@ -6,28 +6,30 @@
 #include <cmath>
 
 template<typename T>
-using Vector3 = Vector<3, T>;
+using Vector4 = Vector<4, T>;
 
-using Vector3f = Vector3<f32>;
+using Vector4f = Vector4<f32>;
 
 template<typename T>
-struct Matrix<3, 1, T> {
+struct Matrix<4, 1, T> {
     union {
         struct {
             T x;
             T y;
             T z;
+            T w;
         };
         struct {
             T r;
             T g;
             T b;
+            T a;
         };
     };
 
-    Matrix<3, 1, T>() : x(T()), y(T()), z(T()) {}
-    Matrix<3, 1, T>(const T& scalar) : x(scalar), y(scalar), z(scalar) {}
-    Matrix<3, 1, T>(const T& x, const T& y, const T& z) : x(x), y(y), z(z) {}
+    Matrix<4, 1, T>() : x(T()), y(T()), z(T()), w(T()) {}
+    Matrix<4, 1, T>(const T& scalar) : x(scalar), y(scalar), z(scalar), w(scalar) {}
+    Matrix<4, 1, T>(const T& x, const T& y, const T& z, const T& w) : x(x), y(y), z(z), w(w) {}
 
     T& operator[](u64 index) {
         switch (index) {
@@ -37,6 +39,8 @@ struct Matrix<3, 1, T> {
                 return this->y;
             case 2:
                 return this->z;
+            case 3:
+                return this->w;
             default:
                 // TODO: Error
                 return this->x;
@@ -51,186 +55,192 @@ struct Matrix<3, 1, T> {
                 return this->y;
             case 2:
                 return this->z;
+            case 3:
+                return this->w;
             default:
                 // TODO: Error
                 return this->x;
         }
     }
 
-    static T Dot(const Vector3<T>& a, const Vector3<T>& b) {
-        return a.x * b.x + a.y * b.y + a.z * b.z;
+    static T Dot(const Vector4<T>& a, const Vector4<T>& b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
     }
 
-    static Vector3<T> Cross(const Vector3<T>& a, const Vector3<T>& b) {
-        return {
-            a.y * b.z - a.z * b.y,
-            a.z * b.x - a.x * b.z,
-            a.x * b.y - a.y * b.x,
-        };
-    }
-
-    static Vector3<T> Normalise(const Vector3<T>& v) {
-        T length = static_cast<T>(sqrt(static_cast<f64>(Vector3<T>::Dot(v, v))));
+    static Vector4<T> Normalise(const Vector4<T>& v) {
+        T length = static_cast<T>(sqrt(static_cast<f64>(Vector4<T>::Dot(v, v))));
         if (length != T()) {
             return v / length;
         } else {
-            return Vector3<T>();
+            return Vector4<T>();
         }
     }
 };
 
 template<typename T>
-Vector3<T> operator+(const Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T> operator+(const Vector4<T>& a, const Vector4<T>& b) {
     return {
         a.x + b.x,
         a.y + b.y,
         a.z + b.z,
+        a.w + b.w,
     };
 }
 
 template<typename T>
-Vector3<T>& operator+=(Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T>& operator+=(Vector4<T>& a, const Vector4<T>& b) {
     a = a + b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator+(const Vector3<T>& a, const T& b) {
+Vector4<T> operator+(const Vector4<T>& a, const T& b) {
     return {
         a.x + b,
         a.y + b,
         a.z + b,
+        a.w + b,
     };
 }
 
 template<typename T>
-Vector3<T> operator+(const T& a, const Vector3<T>& b) {
+Vector4<T> operator+(const T& a, const Vector4<T>& b) {
     return {
         a + b.x,
         a + b.y,
         a + b.z,
+        a + b.w,
     };
 }
 
 template<typename T>
-Vector3<T> operator+=(Vector3<T>& a, const T& b) {
+Vector4<T> operator+=(Vector4<T>& a, const T& b) {
     a = a + b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator-(const Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T> operator-(const Vector4<T>& a, const Vector4<T>& b) {
     return {
         a.x - b.x,
         a.y - b.y,
         a.z - b.z,
+        a.w - b.w,
     };
 }
 
 template<typename T>
-Vector3<T>& operator-=(Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T>& operator-=(Vector4<T>& a, const Vector4<T>& b) {
     a = a - b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator-(const Vector3<T>& a, const T& b) {
+Vector4<T> operator-(const Vector4<T>& a, const T& b) {
     return {
         a.x - b,
         a.y - b,
         a.z - b,
+        a.w - b,
     };
 }
 
 template<typename T>
-Vector3<T> operator-(const T& a, const Vector3<T>& b) {
+Vector4<T> operator-(const T& a, const Vector4<T>& b) {
     return {
         a - b.x,
         a - b.y,
         a - b.z,
+        a - b.w,
     };
 }
 
 template<typename T>
-Vector3<T> operator-=(Vector3<T>& a, const T& b) {
+Vector4<T> operator-=(Vector4<T>& a, const T& b) {
     a = a - b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator*(const Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T> operator*(const Vector4<T>& a, const Vector4<T>& b) {
     return {
         a.x * b.x,
         a.y * b.y,
         a.z * b.z,
+        a.w * b.w,
     };
 }
 
 template<typename T>
-Vector3<T>& operator*=(Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T>& operator*=(Vector4<T>& a, const Vector4<T>& b) {
     a = a * b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator*(const Vector3<T>& a, const T& b) {
+Vector4<T> operator*(const Vector4<T>& a, const T& b) {
     return {
         a.x * b,
         a.y * b,
         a.z * b,
+        a.w * b,
     };
 }
 
 template<typename T>
-Vector3<T> operator*(const T& a, const Vector3<T>& b) {
+Vector4<T> operator*(const T& a, const Vector4<T>& b) {
     return {
         a * b.x,
         a * b.y,
         a * b.z,
+        a * b.w,
     };
 }
 
 template<typename T>
-Vector3<T> operator*=(Vector3<T>& a, const T& b) {
+Vector4<T> operator*=(Vector4<T>& a, const T& b) {
     a = a * b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator/(const Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T> operator/(const Vector4<T>& a, const Vector4<T>& b) {
     return {
         a.x / b.x,
         a.y / b.y,
         a.z / b.z,
+        a.w / b.w,
     };
 }
 
 template<typename T>
-Vector3<T>& operator/=(Vector3<T>& a, const Vector3<T>& b) {
+Vector4<T>& operator/=(Vector4<T>& a, const Vector4<T>& b) {
     a = a / b;
     return a;
 }
 
 template<typename T>
-Vector3<T> operator/(const Vector3<T>& a, const T& b) {
+Vector4<T> operator/(const Vector4<T>& a, const T& b) {
     return {
         a.x / b,
         a.y / b,
         a.z / b,
+        a.w / b,
     };
 }
 
 template<typename T>
-Vector3<T> operator/(const T& a, const Vector3<T>& b) {
+Vector4<T> operator/(const T& a, const Vector4<T>& b) {
     return {
         a / b.x,
         a / b.y,
         a / b.z,
+        a / b.w,
     };
 }
 
 template<typename T>
-Vector3<T> operator/=(Vector3<T>& a, const T& b) {
+Vector4<T> operator/=(Vector4<T>& a, const T& b) {
     a = a / b;
     return a;
 }
